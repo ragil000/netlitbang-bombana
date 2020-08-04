e0ecb0b499fc0545c7adc0e6108739edfbb2ce77
Ragil Manggalaning <ragilmanggalaning42@gmail.com>
2020-08-05 06:49:42 +0700

    masuk di profil

--- client/application/views/templates/footer.php
+++ client/application/views/templates/footer.php
@@ -31,23 +31,21 @@
                     <div class="col-lg-3 col-md-6 footer-contact">
                         <h4>Kontak Kami</h4>
                         <p>
-                        A108 Adam Street <br>
-                        New York, NY 535022<br>
-                        United States <br><br>
-                        <strong>Phone:</strong> +1 5589 55488 55<br>
-                        <strong>Email:</strong> info@example.com<br>
+                        -<br>
+                        <strong>Phone:</strong> -<br>
+                        <strong>Email:</strong> netlitbang@gmail.com<br>
                         </p>
 
                     </div>
 
                     <div class="col-lg-3 col-md-6 footer-info">
                         <h3>Tentang Netlitbang</h3>
-                        <p>Cras fermentum odio eu feugiat lide par naso tierra. Justo eget nada terra videa magna derita valies darta donna mare fermentum iaculis eu non diam phasellus.</p>
+                        <p>Website informasi dan publikasi kelitbangan kabupaten Bombana. Website ini menyediakan informasi aktifitas dan kegiatan litbang kabupaten Bombana. Juga sebagai pusat publikasi kelitbangan dan usulan penelitian kabupaten Bombana.</p>
                         <div class="social-links mt-3">
-                            <a href="#" class="twitter"><i class="bx bxl-twitter"></i></a>
+                            <!-- <a href="#" class="twitter"><i class="bx bxl-twitter"></i></a>
                             <a href="#" class="facebook"><i class="bx bxl-facebook"></i></a>
                             <a href="#" class="instagram"><i class="bx bxl-instagram"></i></a>
-                            <a href="#" class="linkedin"><i class="bx bxl-linkedin"></i></a>
+                            <a href="#" class="linkedin"><i class="bx bxl-linkedin"></i></a> -->
                         </div>
                     </div>
 
